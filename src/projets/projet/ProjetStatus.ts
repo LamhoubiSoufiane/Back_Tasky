35b9dc17e309810647d38375d269c73592900697
SalahAmimi <salahamimi0@gmail.com>
2025-02-04 15:00:20 +0100

    Résolution des conflits avec develop

--- src/projets/projet/ProjetStatus.ts
+++ src/projets/projet/ProjetStatus.ts
@@ -1,7 +1,8 @@
 export enum ProjetStatus {
-    EN_COURS = 'EN_COURS',
-    TERMINE = 'TERMINE',
-    ANNULE = 'ANNULE',
-    PLANIFIE = 'PLANIFIE'
+    Planifie = 'planifié',
+    EnCours = 'en cours',
+    Suspendu = 'suspendu',
+    Termine = 'terminé',
+    Annule = 'annulé',
 }
   
